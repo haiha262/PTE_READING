5bb0a64cad521898670f44ab6d87f07f282799b7
Ha Tran <haiha262@gmail.com>
2018-10-30 23:42:55 +1100

    add list page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 
 $(document).ready(function(){
-  
- var question= 0;
 
-    
-    
-    function getAnswer(str) {
-      var start = str.search("Answer");
-      var end = str.lastIndexOf("");
-      var content = str.substring(start, end);
-      document.getElementById("answer").innerHTML = content;
-      return str.replace(content, ""); 
+var global_question= 0;
 
-    }
+
+
+
+
+ 
+function getAnswer(str) {
+  var start = str.search("Answer");
+  var end = str.lastIndexOf("");
+  var content = str.substring(start, end);
+  document.getElementById("answer").innerHTML = content;
+  return str.replace(content, ""); 
+
+}
+
 
 function AddAnswerButton() {
     if ($("#showAnswer").length > 0){
@@ -56,40 +60,97 @@ function question_process(str)
         document.getElementById("div1").innerHTML = str;
 }
 
-
-    $("#start").click(function(){
-
-      var str ="";
+function nextQuestion(question)
+{
+  var str ="";
       document.getElementById("answer");
     document.getElementById("answer").style.display = "none";;
-      var xhr=new XMLHttpRequest();
-      var link = "https://raw.githubusercontent.com/haiha262/PTE_READING/master/data/RW_"+(++question)+".txt";
-      xhr.open("GET",link);
-      xhr.onload=function(){
-        var text = xhr.responseText;
-        if (text.search("404") !=-1)
-        {
-          question = 0;
-          document.getElementById("div1").innerHTML = "Start Again";
-          document.getElementById("start").innerHTML = "Start Again";
-          $("#showAnswer").remove();
-        }
-        else
-        {
-          str = text;
-          document.getElementById("start").innerHTML = "Next";
-          question_process (str);
-        }
-      };
-      xhr.send();
-        
+    var useXHR = true;
+      if (useXHR)
+      {
+        var xhr=new XMLHttpRequest();
+        var link = "https://raw.githubusercontent.com/haiha262/PTE_READING/master/data/RW_"+(question)+".txt";
+        xhr.open("GET",link);
+        xhr.onload=function(){
+          var text = xhr.responseText;
+          if (text.search("404") !=-1)
+          {
+            global_question = 1;
 
-        
-    });
 
-  
+            document.getElementById("div1").innerHTML = "Start Again";
+            document.getElementById("start").innerHTML = "Start Again";
+            $("#showAnswer").remove();
+            //hide list page
+            var x = document.getElementById("page_number");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } 
+          }
+          else
+          {
+            str = text;
+            document.getElementById("start").innerHTML = "Next";
+            question_process (str);
+            global_question = question;
+          }
+        };
+        xhr.send();
+      }
+      else
+      {
+        readTextFile("./data/RW_1.txt");
+        document.getElementById("start").innerHTML = "Next";
+            question_process (str);
+      }
+}
+
+$("#start").click(function(){
+
+  //show list page
+  var x = document.getElementById("page_number");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } 
+
+  global_question = global_question+1;
+  nextQuestion(global_question);
+
+
+    
+});
+
+listPage();
+function listPage()
+{
+  var number_page = 100;
+  var ul_tag = document.getElementById("list_page");
+  for (var i = 1; i <=  number_page; i++) {
  
+    var li_tag = document.createElement("li");
+    var a_tag = document.createElement("a");
+    var name = document.createTextNode(i);
+    a_tag.appendChild(name);
+    clickEvent(a_tag,i);
+    li_tag.appendChild(a_tag);
+    ul_tag.appendChild(li_tag);
+  }
+}
+function clickEvent(o,x)
+{
+ o.onclick=function(){ nextQuestion(x); }
+}
   
+  function readTextFile(filepath) {
+    var str = "";
+    var txtFile = new File(filepath);
+    txtFile.open("r");
+    while (!txtFile.eof) {
+    // read each line of text
+    str += txtFile.readln() + "\n";
+    }
+    return str;
+  }
   
 
   
