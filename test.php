bb37da406e2cae804551113636367097a68cb570
Ha Tran <haiha262@gmail.com>
2018-11-30 15:05:38 +1100

    test php

--- test.php
+++ test.php
@@ -1,33 +1,9 @@
 <html>
 <body>
 <?php
-$a = "hello";
+phpinfo();
 ?>
-<script>
-    $total_line = 1
-    function echoHello($file_source)
-    {
-        <?php
-        $file = fopen("./data/RW".$file_source, "r") or exit("Unable to open file!");
-        //Output a line of the file until the end is reached
-        $total_line = 0;
-        $line_arr = array();
-        while(!feof($file)) {
-            $line = fgets($file);
-            if($line != null)
-                $line_arr[$total_line++] = $line;
-        }
-        fclose($file);
-        ?>
-    }
-</script>
-<?php
 
-function hello() {
-    global $a;
-    $file_source++ ;
-}
-?>
-<button onclick="echoHello()">Next</button>
+
 </body>
 </html>
